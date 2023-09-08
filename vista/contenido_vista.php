7a9195b1b2866845a31f8872e69f16c64ddc822d
Wilson Vasquez <wilsonraymundov@gmail.com>
2023-09-08 07:30:02 -0600

    first commit

---
+++ vista/contenido_vista.php
@@ -0,0 +1,25 @@
+<?php include 'plantillas/header.php'; ?>
+	<section class="main">
+		<div class="wrapp">
+			<?php include 'plantillas/nav.php'; ?>
+			
+				<article>
+					<div class="mensaje">
+						<h2>Sistema de Gestion de Centro Medico</h2>
+					</div>
+						<p><img src="img/icono.svg">
+						</p><br/><br/>
+						<p>Bienvenido a <b>Centro Medico Tierra Nueva</b>! un Sistema para agilizar cada uno de los procesos</p>
+						<br/><br/>
+						<h3>Nuestras funciones</h3><br/>
+						<p>	- Gestion de Citas <br/>
+							- Gestion de Medicos <br/>
+							- Gestion de Pacientes <br/>
+							- Gestion de Consultorios <br/>
+							- Gestion de Usuarios con Acceso al Sistema <br/>
+						</p>
+				</article>
+	</section>
+	<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
---
+++ vista/plantillas/footer.php
@@ -0,0 +1,3 @@
+	<footer>
+				<p>Copyright © 2023 <a target="_blank": href="../index.php" title="Centro Medico">Cetro de Salud Tierra Nueva</a></p>
+	</footer>
